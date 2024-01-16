0e181253a1ef1ce2b021c7f4dcc31f351c2aa126
sumonhasan55 <wubsumon7577@gmail.comm>
2024-01-16 17:10:58 +0600

    first commit

---
+++ src/pages/testimonial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const testimonialPage = () => {
+    return (
+        <section className=' h-screen'>
+            <h1 className=' text-center text-2xl text-primary font-sans my-auto '>Thank You Everyone!!!</h1>
+            
+        </section>
+    );
+};
+
+export default testimonialPage;
